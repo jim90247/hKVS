b4747305cbc5177eb0fc83e2e0c489e5a2d94e1f
jim90247 <jim90247@gmail.com>
2021-12-14 17:26:57 +0800

    Add a LRU record with min_count constraint

--- src/util/lru_records.h
+++ src/util/lru_records.h
@@ -2,6 +2,7 @@
 #include <list>
 #include <optional>
 #include <unordered_map>
+#include <queue>
 
 template <typename Record>
 class LruRecords {
@@ -22,15 +23,16 @@ class LruRecords {
    * @param record the record
    * @return the removed record
    */
-  inline std::optional<Record> Put(const Record &record) {
+  virtual inline std::optional<Record> Put(const Record &record) {
+    items_.push_front(record);
     auto it = map_.find(record);
     if (it != map_.end()) {
       // remove old copy
       items_.erase(it->second);
-      map_.erase(it);
+      it->second = items_.begin();
+    } else {
+      map_[record] = items_.begin();
     }
-    items_.push_front(record);
-    map_[record] = items_.begin();
     if (items_.size() > capacity_) {
       ListIterator last = std::prev(items_.end());
       Record popped = *last;
@@ -47,7 +49,7 @@ class LruRecords {
    * @param record the record
    * @return true if the record exists
    */
-  inline bool Contain(const Record &record) const {
+  virtual inline bool Contain(const Record &record) const {
     return map_.find(record) != map_.end();
   }
 
@@ -55,4 +57,76 @@ class LruRecords {
   const size_t capacity_;
   std::unordered_map<Record, ListIterator> map_;
   std::list<Record> items_;
+};
+
+/**
+ * @brief An extended LRU record with an additional constraint: records are
+ * included only if they appear in a sliding window multiple times.
+ *
+ * @tparam T the LRU record type
+ */
+template <typename T>
+class LruRecordsWithMinCount : public LruRecords<T> {
+ public:
+  /**
+   * @brief Construct an empty LRU Records With a fixed capacity and threshold
+   * for minimum occurences in a sliding window.
+   *
+   * @param cap the LRU record capacity
+   * @param win the size of the sliding window
+   * @param min_cnt the minimum occurences in a sliding window
+   */
+  LruRecordsWithMinCount(size_t cap, size_t win, int min_cnt)
+      : LruRecords<T>(cap), window_size_(win), min_count_(min_cnt) {}
+  LruRecordsWithMinCount(const LruRecordsWithMinCount &) = delete;
+  LruRecordsWithMinCount &operator=(const LruRecordsWithMinCount &) = delete;
+  /**
+   * @brief Inserts a record into the LRU record. May remove the oldest record
+   * to meet the capacity restriction. A record will be actually inserted only
+   * if it appears not less than min_cnt times in current window.
+   *
+   * @param record the record
+   * @return the removed record
+   */
+  virtual inline std::optional<T> Put(const T &record) override {
+    window_.push(record);
+    IncrementAppearance(record);
+    if (window_.size() >= window_size_) {
+      DecrementAppearance(window_.front());
+      window_.pop();
+    }
+
+    if (counts_[record] >= min_count_) {
+      return LruRecords<T>::Put(record);
+    } else {
+      return std::nullopt;
+    }
+  }
+
+ private:
+  const size_t window_size_;
+  const int min_count_;
+  std::unordered_map<T, int> counts_;
+  std::queue<T> window_;
+
+  void IncrementAppearance(const T &key) {
+    auto it = counts_.find(key);
+    if (it == counts_.end()) {
+      counts_[key] = 1;
+    } else {
+      it->second++;
+    }
+  }
+
+  void DecrementAppearance(const T &key) {
+    auto it = counts_.find(key);
+    if (it == counts_.end()) {
+      return;
+    }
+    if (it->second == 1) {
+      counts_.erase(it);
+    } else {
+      it->second--;
+    }
+  }
 };
