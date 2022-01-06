b15936c1a3a8b836a473287c92574f250dddc976
jim90247 <jim90247@gmail.com>
2022-01-06 18:31:28 +0800

    Fix build/execution errors

--- src/mica/test.sh
+++ src/mica/test.sh
@@ -1,18 +1,20 @@
+#!/bin/bash
+
 # A function to echo in blue color
 function blue() {
-	es=`tput setaf 4`
-	ee=`tput sgr0`
-	echo "${es}$1${ee}"
+    es=$(tput setaf 4)
+    ee=$(tput sgr0)
+    echo "${es}$1${ee}"
 }
 
 blue "Removing SHM keys used by MICA"
-for i in `seq 0 16`; do
-	key=`expr 3185 + $i`
-	sudo ipcrm -M $key 2>/dev/null
-	key=`expr 4185 + $i`
-	sudo ipcrm -M $key 2>/dev/null
+for i in $(seq 0 16); do
+    key=$((3185 + i))
+    sudo ipcrm -M $key 2>/dev/null
+    key=$((4185 + i))
+    sudo ipcrm -M $key 2>/dev/null
 done
 
 num_threads=1
-sudo numactl --cpunodebind=0 --membind=0 ./test \
-	--num-threads $num_threads
+numactl --cpunodebind=0 --membind=0 ./test \
+    --num-threads $num_threads
