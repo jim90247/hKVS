1100b4a1f8cb3d96a16c0cd89ea2d801b8ce446a
jim90247 <jim90247@gmail.com>
2022-01-12 13:41:14 +0800

    Add more parameters and save log to file

--- exp/util/lru_coverage.cc
+++ exp/util/lru_coverage.cc
@@ -9,13 +9,14 @@
 #include "util/lru_records.h"
 #include "util/zipfian_generator.h"
 
-DEFINE_uint64(lru_size, 8192UL, "Size of LRU record");
+DEFINE_int64(lru_size, 8192L, "Size of LRU record");
 DEFINE_uint64(trace_size, 16UL << 20, "Length of trace");
 DEFINE_double(zipfian_alpha, 0.99, "Zipfian parameter");
 DEFINE_uint64(key_range, 8UL << 20, "Key range");
 DEFINE_uint64(window_size, 64UL << 10, "Time window size");
 DEFINE_uint64(window_min_count, 4,
               "Minimum amount of appearance to be placed in cache");
+DEFINE_int32(seed, 42, "Random seed for trace generation");
 
 /**
  * @brief Generates a key access trace based on Zipfian distribution. Key range:
@@ -37,7 +38,7 @@ std::vector<int> GenerateZipfianTrace(int seed) {
 int main(int argc, char** argv) {
   using clock = std::chrono::steady_clock;
   FLAGS_colorlogtostderr = true;
-  FLAGS_logtostderr = true;
+  FLAGS_alsologtostderr = true;
   gflags::ParseCommandLineFlags(&argc, &argv, true);
   google::InitGoogleLogging(argv[0]);
   google::InstallFailureSignalHandler();
@@ -46,7 +47,7 @@ int main(int argc, char** argv) {
   // LruRecords<int> cache(FLAGS_lru_size);
 
   auto start = clock::now();
-  auto trace = GenerateZipfianTrace(42);
+  auto trace = GenerateZipfianTrace(FLAGS_seed);
   auto end = clock::now();
   LOG(INFO) << "Generate zipfian trace of size " << FLAGS_trace_size
             << " takes " << std::chrono::duration<double>(end - start).count()
@@ -70,6 +71,14 @@ int main(int argc, char** argv) {
             << " op/s";
   LOG(INFO) << "Hit rate = " << static_cast<double>(hit) / FLAGS_trace_size
             << ", Eviction = " << evict;
+
+  LOG(INFO) << "Record=" << FLAGS_lru_size << "," << FLAGS_trace_size << ","
+            << FLAGS_zipfian_alpha << "," << FLAGS_key_range << ","
+            << FLAGS_window_size << "," << FLAGS_window_min_count << ","
+            << FLAGS_seed << "," << static_cast<double>(hit) / FLAGS_trace_size
+            << "," << evict << ","
+            << FLAGS_trace_size /
+                   std::chrono::duration<double>(end - start).count();
 
   hit = 0;
   for (auto x : trace) {
