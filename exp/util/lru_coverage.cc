c85ab5741036e5f0f011410ab3ebb7528db39d0c
jim90247 <jim90247@gmail.com>
2022-01-13 17:57:10 +0800

    Test LRU coverage with Twitter trace

--- exp/util/lru_coverage.cc
+++ exp/util/lru_coverage.cc
@@ -6,31 +6,37 @@
 #include <unordered_map>
 #include <vector>
 
+#include "csv.hpp"
 #include "util/lru_records.h"
+#include "util/twitter_trace_reader.h"
 #include "util/zipfian_generator.h"
 
+/// LRU params
 DEFINE_int64(lru_size, 8192L, "Size of LRU record");
-DEFINE_uint64(trace_size, 16UL << 20, "Length of trace");
-DEFINE_double(zipfian_alpha, 0.99, "Zipfian parameter");
-DEFINE_uint64(key_range, 8UL << 20, "Key range");
 DEFINE_uint64(window_size, 64UL << 10, "Time window size");
 DEFINE_uint64(window_min_count, 4,
               "Minimum amount of appearance to be placed in cache");
+/// Common trace params
+DEFINE_uint64(trace_size, 16UL << 20, "Length of trace to read/generate");
+/// Twitter cache trace params
+DEFINE_string(twttr_trace, "", "Path to Twitter cache trace CSV");
+/// Generator params
+DEFINE_double(zipfian_alpha, 0.99, "Zipfian parameter");
+DEFINE_uint64(key_range, 8UL << 20, "Key range");
 DEFINE_int32(seed, 42, "Random seed for trace generation");
 
-/**
- * @brief Generates a key access trace based on Zipfian distribution. Key range:
- * [0, FLAGS_key_range).
- *
- * @param seed the random seed
- * @return a vector of integers in range [0, FLAGS_key_range) representing the
- * trace
- */
-std::vector<int> GenerateZipfianTrace(int seed) {
-  ZipfianGenerator gen(FLAGS_key_range, FLAGS_zipfian_alpha, seed);
+std::vector<int> BuildTrace() {
   std::vector<int> trace(FLAGS_trace_size);
-  for (size_t i = 0; i < FLAGS_trace_size; i++) {
-    trace[i] = gen.GetNumber();
+  if (FLAGS_twttr_trace.empty()) {
+    ZipfianGenerator gen(FLAGS_key_range, FLAGS_zipfian_alpha, FLAGS_seed);
+    for (size_t i = 0; i < FLAGS_trace_size; i++) {
+      trace[i] = gen.GetNumber();
+    }
+  } else {
+    TwttrTraceReader trace_reader(FLAGS_twttr_trace);
+    for (size_t i = 0; i < FLAGS_trace_size; i++) {
+      trace[i] = trace_reader.GetNumber();
+    }
   }
   return trace;
 }
@@ -46,15 +52,11 @@ int main(int argc, char** argv) {
                                     FLAGS_window_min_count);
   // LruRecords<int> cache(FLAGS_lru_size);
 
-  auto start = clock::now();
-  auto trace = GenerateZipfianTrace(FLAGS_seed);
-  auto end = clock::now();
-  LOG(INFO) << "Generate zipfian trace of size " << FLAGS_trace_size
-            << " takes " << std::chrono::duration<double>(end - start).count()
-            << " seconds";
+  std::vector<int> trace = BuildTrace();
+
   LOG(INFO) << "Starting benchmark";
-  start = clock::now();
-  unsigned long hit = 0, evict = 0;
+  auto start = clock::now();
+  size_t hit = 0, evict = 0;
   for (size_t i = 1; i <= FLAGS_trace_size; i++) {
     int key = trace.at(i - 1);
     if (cache.Contain(key)) {
@@ -64,29 +66,35 @@ int main(int argc, char** argv) {
       evict++;
     }
   }
-  end = clock::now();
-  LOG(INFO) << "Performance = "
-            << FLAGS_trace_size /
-                   std::chrono::duration<double>(end - start).count()
-            << " op/s";
-  LOG(INFO) << "Hit rate = " << static_cast<double>(hit) / FLAGS_trace_size
-            << ", Eviction = " << evict;
+  auto end = clock::now();
 
-  LOG(INFO) << "Record=" << FLAGS_lru_size << "," << FLAGS_trace_size << ","
-            << FLAGS_zipfian_alpha << "," << FLAGS_key_range << ","
-            << FLAGS_window_size << "," << FLAGS_window_min_count << ","
-            << FLAGS_seed << "," << static_cast<double>(hit) / FLAGS_trace_size
-            << "," << evict << ","
-            << FLAGS_trace_size /
-                   std::chrono::duration<double>(end - start).count();
+  double ops =
+      FLAGS_trace_size / std::chrono::duration<double>(end - start).count();
+  double hit_rate = static_cast<double>(hit) / FLAGS_trace_size;
+  LOG(INFO) << "Performance = " << ops << " op/s";
+  LOG(INFO) << "Hit rate = " << hit_rate << ", Eviction = " << evict;
 
-  hit = 0;
-  for (auto x : trace) {
-    if (x < FLAGS_lru_size) {
-      hit++;
+  std::stringstream ss;
+  auto csv_writer = csv::make_csv_writer(ss);
+  if (FLAGS_twttr_trace.empty()) {
+    hit = 0;
+    for (auto x : trace) {
+      if (x < FLAGS_lru_size) {
+        hit++;
+      }
     }
+    LOG(INFO) << "Optimal static offload = "
+              << static_cast<double>(hit) / FLAGS_trace_size;
+
+    csv_writer << std::make_tuple(FLAGS_lru_size, FLAGS_trace_size,
+                                  FLAGS_zipfian_alpha, FLAGS_key_range,
+                                  FLAGS_window_size, FLAGS_window_min_count,
+                                  FLAGS_seed, hit_rate, evict, ops);
+  } else {
+    csv_writer << std::make_tuple(FLAGS_lru_size, FLAGS_trace_size,
+                                  FLAGS_twttr_trace, FLAGS_window_size,
+                                  FLAGS_window_min_count, hit_rate, evict);
   }
-  LOG(INFO) << "Optimal static offload = "
-            << static_cast<double>(hit) / FLAGS_trace_size;
+  LOG(INFO) << "Record=" << ss.str();
   return 0;
 }
