b0a27a80e68b56b5fdc56501f4572e8f73497287
jim90247 <jim90247@gmail.com>
2022-02-21 21:36:26 +0800

    Print avg and median instead of the one before the last

--- scripts/combined/herd_tput.py
+++ scripts/combined/herd_tput.py
@@ -3,7 +3,7 @@ import re
 from argparse import ArgumentParser
 from collections import defaultdict
 from pprint import pprint
-from statistics import median
+from statistics import median, mean
 
 # example_str = "main: Worker 1: 6605767.48 IOPS. Avg per-port postlist = 17.81. HERD lookup fail rate = 0.0085"
 
@@ -19,24 +19,27 @@ def parse_line(line: str):
 def main(args):
     with open(args.log) as f:
         lines = f.read().splitlines()
-    worker_stats = defaultdict(lambda: defaultdict(list))
+    iops_stats = defaultdict(lambda: [])
+    postlist_stats = defaultdict(lambda: [])
     for line in lines:
         stats = parse_line(line)
         if stats is None:
             continue
-        for stat in ('iops', 'postlist'):
-            worker_stats[stats['worker_id']][stat].append(float(stats[stat]))
+        worker_id = int(stats['worker_id'])
+        iops_stats[worker_id].append(float(stats['iops']))
+        postlist_stats[worker_id].append(float(stats['postlist']))
 
-    worker_stats_median = {}
-    for worker_id, stats in worker_stats.items():
-        worker_stats_median[worker_id] = {name: values[-2] for name, values in stats.items()}
-    pprint(worker_stats_median)
-
-    median_total = defaultdict(lambda: 0)
-    for worker_id, stats in worker_stats_median.items():
-        for name, value in stats.items():
-            median_total[name] += value
-    pprint(dict(median_total))
+    total_iops_median = 0.0
+    total_iops_avg = 0.0
+    for worker_id in sorted(iops_stats):
+        x = median(iops_stats[worker_id])
+        y = mean(iops_stats[worker_id])
+        print("worker {:2d}: median={:.2f} op/s, avg={:.2f} op/s, avg postlist={:.2f}".format(
+            worker_id, x, y, mean(postlist_stats[worker_id])))
+        total_iops_median += x
+        total_iops_avg += y
+        pass
+    print("median={:.2f} op/s, avg={:.2f} op/s".format(total_iops_median, total_iops_avg))
 
 
 if __name__ == '__main__':
